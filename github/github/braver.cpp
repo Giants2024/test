6af26fb8bd1f9d8d96cfbd2a7ca914e9a7063840
Giants <2682866322@qq.com>
2024-10-27 18:52:50 +0800

    00

--- github/github/braver.cpp
+++ github/github/braver.cpp
@@ -1,10 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main()
-{
-	int a = 0, b = 0 ,sum = 0;
-	scanf("%d%d", &a, &b);
-	sum = a + b;
-	printf("sum=%d\n", sum);
-
-}
+//int main()
+//{
+//	int a = 0;
+//	printf("请输入一个年份\n");
+//	scanf("%d", &a);
+//	if (a % 4 != 0)
+//		printf("平年\n");
+//	else if (a % 400 == 0)
+//		printf("闰年\n");
+//	else if (a % 100 == 0)
+//		printf("平年\n");
+//	else
+//		printf("闰年\n");
+//
+//
+//	return 0;
+//}
